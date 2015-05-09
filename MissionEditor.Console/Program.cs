d52eb8ae44a3d78a0f79fca218edd23389b5b512
abcdefg30 <abcdefg30@users.noreply.github.com>
2015-05-10 00:35:59 +0200

    Add parameter support to MissionEditor.Console

--- MissionEditor.Console/Program.cs
+++ MissionEditor.Console/Program.cs
@@ -1,7 +1,4 @@
-﻿using System.IO;
-using System.Runtime.InteropServices;
-using System.Runtime.Serialization.Formatters.Binary;
-using MissionEditor.FileReaderCore;
+﻿using MissionEditor.FileReaderCore;
 
 namespace MissionEditor.Console
 {
@@ -9,44 +6,14 @@ namespace MissionEditor.Console
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+                return;
 
-            var filePath = @"E:\Work\Programming\C#\OpenRA\Dune2000\Resources\MISSIONS\_A5V1.MIS";
-
-            //var bytes = File.ReadAllBytes(filePath);
-            //var mission = new MissionStruct();
-            //var m = ByteArrayToStructure(bytes);
-
+            var filePath = args[0];
+            var outputName = args.Length > 1 ? args[1] : "testDump.txt";
 
             var mission = FileReaderCore.FileReader.ParseMissionFile(filePath);
-
-            FileReaderCore.FileReader.DumpDataToFile(mission, "testDump.txt");
-
-            return;
-
-            var miss = ByteArrayToStructure(File.ReadAllBytes(filePath));
-
-            //var fs = File.Open(filePath, FileMode.Open);
-            //var binaryFormatter = new BinaryFormatter();
-            //MissionTest mis = (MissionTest)binaryFormatter.Deserialize(fs);
-
+            FileReaderCore.FileReader.DumpDataToFile(mission, outputName);
         }
-
-        static MissionTest ByteArrayToStructure(byte[] bytes)
-        {
-            var handle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
-            var stuff = (MissionTest)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(MissionTest));
-            handle.Free();
-            return stuff;
-        }
-
-        object ByteArrayToStructure(byte[] bytearray, object structureObj, int position)
-        {
-            var length = Marshal.SizeOf(structureObj);
-            var ptr = Marshal.AllocHGlobal(length);
-            Marshal.Copy(bytearray, 0, ptr, length);
-            structureObj = Marshal.PtrToStructure(Marshal.UnsafeAddrOfPinnedArrayElement(bytearray, position), structureObj.GetType());
-            Marshal.FreeHGlobal(ptr);
-            return structureObj;
-        }   
     }
 }
